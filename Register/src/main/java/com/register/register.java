3c30322906d2648149061d029db379e7a4911b0f
asmit <singhasmit8083@gmail.com>
2023-09-11 23:08:51 +0530

    issue resolved by adding jar file to tomacat library hence fully working application

--- Register/src/main/java/com/register/register.java
+++ Register/src/main/java/com/register/register.java
@@ -1,56 +1,46 @@
-package com.registration;
+package com.register;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.sql.*;
-import java.io.*;
 
-
-public class register extends HttpServlet {
+public class register extends HttpServlet implements Servlet {
 	private static final long serialVersionUID = 1L;
-       
-    
+
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.println("<body>");
 		
 		String name= request.getParameter("name");
 		String email= request.getParameter("email");
 		String pass= request.getParameter("password");
-		
-	try {
-		Class.forName("com.mysql.jdbc.Driver");
-		
-		String url="jdbc:mysql://localhost:3306/customer";
-		String user="root";
-		String password="Asmit123@";
-		
-		Connection connection= DriverManager.getConnection(url,user,password);
-		System.out.println("connection");
-		String q = "Insert into user(user_name,pass,email) values(?,?,?)";
+
+		try {
+			
+			Thread.sleep(3000);
+			Connection connection = DatabaseConnection.initializeDatabase();
+			System.out.println("inserting statements");
+			String q = "Insert into user(user_name,pass,email) values(?,?,?)";
 			PreparedStatement pstmt= connection.prepareStatement(q);
-			out.println("<h1>done12.....</h1>");
 			pstmt.setString(1,name);
 			pstmt.setString(2, pass);
 			pstmt.setString(3, email);
 			
 			pstmt.executeUpdate();
-			
-			out.println("<h1>done11.....</h1>");
+			out.println("done");
 			connection.close();
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			System.out.println(e);
+			out.println("error");
 		}
-		
-		
-		
-		out.println("</body>");
 	}
 
 }

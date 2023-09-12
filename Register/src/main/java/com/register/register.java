60769585b0235626a81c49ccec2aea1dfe12631c
asmit <singhasmit8083@gmail.com>
2023-09-12 12:04:57 +0530

    added functionality to add images from form and store in database

--- Register/src/main/java/com/register/register.java
+++ Register/src/main/java/com/register/register.java
@@ -1,15 +1,23 @@
 package com.register;
 
 import jakarta.servlet.http.HttpServlet;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
 import java.sql.*;
 
+@MultipartConfig
 public class register extends HttpServlet implements Servlet {
 	private static final long serialVersionUID = 1L;
 
@@ -21,19 +29,33 @@ public class register extends HttpServlet implements Servlet {
 		String name= request.getParameter("name");
 		String email= request.getParameter("email");
 		String pass= request.getParameter("password");
+		Part part= request.getPart("image");
+		String fileName = part.getSubmittedFileName();
 
 		try {
 			
 			Thread.sleep(3000);
 			Connection connection = DatabaseConnection.initializeDatabase();
 			System.out.println("inserting statements");
-			String q = "Insert into user(user_name,pass,email) values(?,?,?)";
+			String q = "Insert into user(user_name,pass,email,image) values(?,?,?,?)";
 			PreparedStatement pstmt= connection.prepareStatement(q);
 			pstmt.setString(1,name);
 			pstmt.setString(2, pass);
 			pstmt.setString(3, email);
+			pstmt.setString(4, fileName);
 			
 			pstmt.executeUpdate();
+			
+			InputStream iStream= part.getInputStream();
+			byte[] data = new byte[iStream.available()];
+			
+			iStream.read(data);
+			
+			String path = request.getServletContext().getRealPath("/")+"images"+File.separator + fileName;
+			FileOutputStream  fos = new FileOutputStream(path);
+			fos.write(data);
+			fos.close();
+			
 			out.println("done");
 			connection.close();
 			
